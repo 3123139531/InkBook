7dc94d4ec03fdef9c5ea10b6b2f660c50e83510a
‘ljx774’ <1060723959@qq.com>
2022-08-04 00:25:06 +0800

    加上了上传文件的接口，并完成了部署

--- src/test/java/com/buaa/service/ProjectServiceTest.java
+++ src/test/java/com/buaa/service/ProjectServiceTest.java
@@ -1,22 +1,22 @@
-package com.buaa.service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class ProjectServiceTest {
-
-    @Autowired
-    private ProjectService  projectService;
-
-    @Test
-    public void testCreate() {
-        projectService.createProject(2, "test");
-    }
-
-    @Test
-    public void testDelete() {
-        projectService.deleteProjectById(3);
-    }
-}
+//package com.buaa.service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class ProjectServiceTest {
+//
+//    @Autowired
+//    private ProjectService projectService;
+//
+//    @Test
+//    public void testCreate() {
+//        projectService.createProject(2, "test");
+//    }
+//
+//    @Test
+//    public void testDelete() {
+//        projectService.deleteProjectById(3);
+//    }
+//}
