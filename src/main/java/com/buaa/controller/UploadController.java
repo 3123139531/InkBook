be96b7d4376791cc3b421c83dbc52de1d951ea07
‘ljx774’ <1060723959@qq.com>
2022-08-04 15:28:34 +0800

    优化了upload方法的配置加载方法

--- src/main/java/com/buaa/controller/UploadController.java
+++ src/main/java/com/buaa/controller/UploadController.java
@@ -1,5 +1,6 @@
 package com.buaa.controller;
 
+import com.buaa.configs.UploadConfig;
 import com.buaa.controller.utils.R;
 import com.qcloud.cos.COSClient;
 import com.qcloud.cos.ClientConfig;
@@ -8,7 +9,7 @@ import com.qcloud.cos.auth.COSCredentials;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.PutObjectResult;
 import com.qcloud.cos.region.Region;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,18 +24,8 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
-    @Value("${cos.accessKey}")
-    private String accessKey;
-    @Value("${cos.secretKey}")
-    private String secretKey;
-    @Value("${cos.regionName}")
-    private String regionName;
-    @Value("${cos.bucketName}")
-    private String bucketName;
-    @Value("${cos.baseUrl}")
-    private String baseUrl;
-    @Value("${cos.folderPrefix}")
-    private String folderPrefix;
+    @Autowired
+    private UploadConfig uploadConfig;
 
     @CrossOrigin
     @PostMapping("/cos")
@@ -54,23 +45,23 @@ public class UploadController {
         int month=cal.get(Calendar.MONTH);
         int day=cal.get(Calendar.DATE);
         // 初始化用户身份信息
-        COSCredentials cred = new BasicCOSCredentials(accessKey, secretKey);
+        COSCredentials cred = new BasicCOSCredentials(uploadConfig.getAccessKey(), uploadConfig.getSecretKey());
         // 设置bucket的区域
-        ClientConfig clientConfig = new ClientConfig(new Region(regionName));
+        ClientConfig clientConfig = new ClientConfig(new Region(uploadConfig.getRegionName()));
         // 生成cos客户端
         COSClient cosclient = new COSClient(cred, clientConfig);
-        String bucketName = this.bucketName;
+        String bucketName = uploadConfig.getBucketName();
 
         File localFile;
         try {
             localFile = File.createTempFile("temp",null);
             file.transferTo(localFile);
             // 指定要上传到cos上的路径
-            String key = "/"+this.folderPrefix+"/"+year+"/"+month+"/"+day+"/"+newFileName;
+            String key = "/"+uploadConfig.getFolderPrefix()+"/"+year+"/"+month+"/"+day+"/"+newFileName;
             PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
             PutObjectResult putObjectResult = cosclient.putObject(putObjectRequest);
-//            System.out.println(this.baseUrl + putObjectRequest.getKey());
-            return new R(true, this.baseUrl + putObjectRequest.getKey(), "上传成功");
+//            System.out.println(uploadConfig.getBaseUrl() + putObjectRequest.getKey());
+            return new R(true, uploadConfig.getBaseUrl() + putObjectRequest.getKey(), "上传成功");
         } finally {
             // 关闭客户端(关闭后台线程)
             cosclient.shutdown();
